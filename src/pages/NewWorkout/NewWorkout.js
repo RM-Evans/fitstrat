64edf2e8480694ea065a545588ed46a36a276f31
Jawsbreaker <evansrichardmichael@gmail.com>
2022-05-13 23:56:12 -0600

    i tried

--- src/pages/NewWorkout/NewWorkout.js
+++ src/pages/NewWorkout/NewWorkout.js
@@ -1,9 +1,16 @@
-import './NewWorkout.css'
+import './NewWorkout.css';
+import Form from 'react-bootstrap/Form';
 
 export default function NewWorkout() {
   return (
     <div className="new-workout-container">
-      <p>there should be a form on this page</p>
+      {/* <p>there should be a form on this page</p> */}
+      <Form >
+        <Form.Group controlId="formBasicEmail" className='formMain'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+      </Form>
     </div>
-  )
+  );
 }
