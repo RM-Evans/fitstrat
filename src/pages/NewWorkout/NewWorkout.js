af7e98eeb7633dd2d3ae3ac8f3f452574f40c520
Michael Evans <evansrichardmichael@gmail.com>
2022-05-19 16:24:54 -0600

    formik

--- src/pages/NewWorkout/NewWorkout.js
+++ src/pages/NewWorkout/NewWorkout.js
@@ -1,16 +1,31 @@
-import './NewWorkout.css';
-import Form from 'react-bootstrap/Form';
+import './NewWorkout.css'
+import Form from 'react-bootstrap/Form'
+import { useFormik } from 'formik'
 
 export default function NewWorkout() {
+  const formik = useFormik({
+    initialValues: {
+      nameOfWorkout: '',
+    },
+  })
+
+  console.log('form values', formik.values)
+
   return (
     <div className="new-workout-container">
       {/* <p>there should be a form on this page</p> */}
-      <Form >
-        <Form.Group controlId="formBasicEmail" className='formMain'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+      <Form>
+        <Form.Group controlId="formBasicEmail" className="formMain">
+          <Form.Label>Workout Name</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Back and Bi, Leg Day, etc..."
+            name="nameOfWorkout"
+            onChange={formik.handleChange}
+            value={formik.values.nameOfWorkout}
+          />
         </Form.Group>
       </Form>
     </div>
-  );
+  )
 }
