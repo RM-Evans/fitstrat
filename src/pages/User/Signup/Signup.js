f0dbc1aa63e0782a68cd9991596dfc14dc2b64d1
Michael Evans <evansrichardmichael@gmail.com>
2022-05-27 17:22:03 -0600

    update

--- src/pages/User/Signup/Signup.js
+++ src/pages/User/Signup/Signup.js
@@ -1,37 +1,44 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useFormik } from 'formik';
-import { db } from '../../../firestore';
-import './Signup.css';
-
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { useFormik } from 'formik'
+import { db } from '../../../firestore'
+import './Signup.css'
 
 export default function Signup() {
-
   const formik = useFormik({
     initialValues: {
       signUpEmail: '',
-      signUpPassword: ''
+      signUpPassword: '',
     },
-    onSubmit: values => {
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+      alert(JSON.stringify(db, null, 2))
 
-      let ref1 = db.database().ref().child('users').push();
-      let key = ref1.key;
-      values.id = key;
-      ref1.set(values);
-      console.log(ref1.key);
-      //  alert(JSON.stringify(values, null, 2));
+      db.collection('users').doc().set(
+        values
+        // {s
+        // name: 'name',
+        // email: 'email',
+        // comment: 'comment',
+        // }
+      )
+
+      // let ref1 = db.database().ref().child('users').push()
+      // let key = ref1.key
+      // values.id = key
+      // ref1.set(values)
+      // console.log(ref1.key)
     },
-  });
+  })
 
-  console.log("hey, listen", db);
-  console.log('form values', formik.values);
-
+  // console.log('hey, listen', db)
+  // console.log('form values', formik.values)
 
   return (
     <div className="signup-container">
       <Form onSubmit={formik.handleSubmit} className="signup-form">
-        <Form.Group controlId="formBasicEmail" className='signup-form-group'>
+        <Form.Group controlId="formBasicEmail" className="signup-form-group">
           <Form.Label className="form-label">Email address</Form.Label>
           <Form.Control
             className="form-input-field"
@@ -43,7 +50,7 @@ export default function Signup() {
           />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword" className='signup-form-group'>
+        <Form.Group controlId="formBasicPassword" className="signup-form-group">
           <Form.Label className="form-label">Password</Form.Label>
           <Form.Control
             className="form-input-field"
@@ -54,12 +61,10 @@ export default function Signup() {
             value={formik.values.signUpPassword}
           />
         </Form.Group>
-        <Button type="submit" className="signup-submit-btn">Signup</Button>
-
+        <Button type="submit" className="signup-submit-btn">
+          Signup
+        </Button>
       </Form>
-
-
-    </div >
-  );
+    </div>
+  )
 }
-
