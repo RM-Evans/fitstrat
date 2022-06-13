0c16b8ef4d707ac799ae683917a29c8dc0afb10f
Michael Evans <evansrichardmichael@gmail.com>
2022-06-12 19:54:24 -0600

    I read a lot of things about firebase databases and about fullcalendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -3,14 +3,24 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+// import scrollGridPlugin from '@fullcalendar/scrollgrid'
 
 export default function Calendar() {
+  // const [dateModal, setDateModal]
   return (
     <div>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[
+          dayGridPlugin,
+          interactionPlugin,
+          listPlugin,
+          // scrollGridPlugin,
+        ]}
         initialView="dayGridWeek"
       />
+
+      {/* <button onClick="open-">addstuff</button> */}
     </div>
   )
 }
